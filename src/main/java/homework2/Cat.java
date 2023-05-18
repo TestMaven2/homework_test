a9e9f1927334902727cef26aab9519367d0d1478
Andrey <k-udm@ya.ru>
2023-05-19 03:31:08 +0400

    Homework 2

---
+++ src/main/java/homework2/Cat.java
@@ -0,0 +1,6 @@
+package homework2;
+
+public class Cat {
+
+    // Здесь домашнее задание к уроку 2
+}
---
+++ src/main/java/homework2/Task2.java
@@ -0,0 +1,6 @@
+package homework2;
+
+public class Task2 {
+
+    // Здесь домашнее задание к уроку 2
+}
